875087df2c59a01a428da3561760ac98433e7b68
jskim <96603612+koratoo@users.noreply.github.com>
2023-04-20 17:59:46 +0900

    230420

--- Spring_project/src/main/java/kr/co/softsoldesk/controller/PointController.java
+++ Spring_project/src/main/java/kr/co/softsoldesk/controller/PointController.java
@@ -1,11 +1,14 @@
 package kr.co.softsoldesk.controller;
 
+import java.text.SimpleDateFormat;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import kr.co.softsoldesk.beans.chargeHistory;
 import kr.co.softsoldesk.mapper.PointMapper;
@@ -30,7 +33,10 @@ public class PointController {
 	public String PointInfo(Model model) {	
 		try {
 			chargeHistory ch = point_mapper.chargehistory();
-			model.addAttribute("ch",ch);			
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			String chargeDateFormatted = dateFormat.format(ch.getCharge_date());
+			ch.setCharge_date(dateFormat.parse(chargeDateFormatted));
+			model.addAttribute("ch", ch);			
 		} catch (Exception e) {
 			System.out.println("예외 발생");
 		}
@@ -46,27 +52,37 @@ public class PointController {
 	}
 	
 	//충전 성공 페이지
-	@GetMapping("/ChargeSuccess")
-	public String ChargeSuccess(chargeHistory ch) {	
-		try {
-			
-			ch.setCharge_amount(121212);
-			ch.setUser_id("kjs002_testdat 230419");
-			ch.setPayment_tool("kakao pay");
-			
-			point_mapper.chargePoint(ch);
-		} catch (Exception e) {
-			System.out.println("충전 페이지 예외 발생");
-		}
-		
-		return "POINT/ChargeSuccess";
-	}
 	
 	
 	@GetMapping("/accountInfo")
 	public String accountInfo() {
 		
 		return "POINT/accountInfo";
+	}
+	
+//	@GetMapping("/ChargeSuccess")
+//	public String ChargeSuccess(chargeHistory ch) {	
+//		try {
+//			
+//			point_mapper.chargePoint(ch);
+//		} catch (Exception e) {
+//			System.out.println("충전 페이지 예외 발생");
+//		}
+//		
+//		return "POINT/ChargeSuccess";
+//	}
+	
+	
+	@GetMapping("/ChargeSuccess/{amount}/{merchant_id}")
+	public String chargeSuccess(@PathVariable int amount, @PathVariable String merchant_id) {    
+	    // amount 값과 merchant_id 값을 이용해 할 일을 처리하는 코드 작성
+	    try {
+	        System.out.println(amount);
+	        System.out.println(merchant_id);
+	    } catch (Exception e) {
+	        // TODO: 예외 처리
+	    }
+	    return "POINT/ChargeSuccess";
 	}
 	
 }
