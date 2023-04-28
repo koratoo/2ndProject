3c59890ef455091752d458bb2f0cad1bf19a548c
jskim <96603612+koratoo@users.noreply.github.com>
2023-04-28 17:18:32 +0900

    Add files via upload

---
+++ Spring_project/0428 백업/Organizations.java
@@ -0,0 +1,13 @@
+package kr.co.itfriend.beans;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Organizations {
+
+	 	private int OG_NO; /* 단체 번호 */
+	    private String OG_NAME;
+	    private int OG_POINT; /* 기부 받은 금액 */
+}
